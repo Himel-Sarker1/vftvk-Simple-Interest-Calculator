717d6d3ee11f0b6b7244c7f5584c2ed452d116f0
Himel Sarker <51125269+Himel-Sarker1@users.noreply.github.com>
2021-05-30 19:05:11 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,27 @@
+var result = document.getElementById("result").value;
 
 function compute()
 {
-    document.getElementById("result").innerHTML="";
-    var principal = document.getElementById("principal").value;
-    if(principal<=0)
-    {
-        alert('Enter a positive number');
-        document.getElementById("principal").focus();
-    }else
-    {
+    var principal = document.getElementById("principal").value; 
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
-    console.log(years);
-    var interest = principal * years * rate /100;
-    console.log(principal*rate);
-var year = new Date().getFullYear()+parseInt(years);
-document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,<br> at an interest rate of <mark>"+rate+"%</mark>. <br>You will receive an amount of <mark>" +interest+"</mark>,<br>in the year <mark>"+year+"</mark><br>";
+    var year = new Date().getFullYear()+parseInt(years);
+    var interest = (principal * years * rate) /100;
+
+    if (principal <= 0)
+    {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    }
+    else
+    {
+        document.getElementById("result").innerHTML = "If you deposit " + "<span class='highlight'>" + principal + "</span>" + "," + "<br>"+ "at an interest rate of " + "<span class='highlight'>" + rate + "%." + "</span>" + "<br>"+ "You will receive an amount of " + "<span class='highlight'>" + interest + "</span>" +"," + "<br>" +"in the year " + "<span class='highlight'>" + year + "</span";
+    }
 }
-}
+
 function updateRate() 
 {
     var rateval = document.getElementById("rate").value;
-                document.getElementById(
-                  "rate_val").innerText =rateval;
-                  document.getElementById("result").innerHTML="";
-
+    document.getElementById("rate_val").innerText=rateval;
 }
-function clearResult()
-{
-    document.getElementById("result").innerHTML="";
-}
+        
